d32bfd158759ce9166faad4ea8e20c4849a94264
gasangw <thomasgasangwa@gmail.com>
2023-07-12 12:09:56 +0200

    Add an image of a card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '5rem',
+        xl: '6rem',
+        '2xl': '7rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
